5292483cff8eff48b1adc47194354dcf717c5654
Chizoba Victory <chizoba.victory1@gmail.com>
2022-12-26 08:10:18 +0100

    edited notifications backend 📱

--- api/src/utils/notification.ts
+++ api/src/utils/notification.ts
@@ -56,7 +56,7 @@ export const eHtml=(otp: number) :string=>{
     let result = `
     <div style = "max-width:700px; margin: auto; border: 10px solid #ddd; padding: 50px, 20px; font-size: 110%;">
     <h2 style = "text-align: center; text-transform: uppercase; color: teal;">
-    Welcome to Victor Store
+    Welcome to Chizoba Victory Food App 
     </h2>
     <p>
     Hi there, your OTP is ${otp}
